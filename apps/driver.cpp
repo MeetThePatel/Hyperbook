8b689a3dd5242657176d6d8b5f4ebf835ffc9e54
Meet S. Patel <meet.s.patel.nj@gmail.com>
2021-01-01 14:07:23 -0500

    Set up a CMake base for the project.

---
+++ apps/driver.cpp
@@ -0,0 +1,6 @@
+#include <hyperbook/hyperbook.h>
+
+int main() {
+  testingFunction("testing");
+  return 0;
+}
---
+++ src/hyperbook.cpp
@@ -0,0 +1,5 @@
+#include <hyperbook/hyperbook.h>
+
+void testingFunction(std::string x) {
+  std::cout << x << '\n';
+}
