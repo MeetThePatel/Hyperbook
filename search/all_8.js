fc90b99c05a2cc10d97d71bcbec82b73e65fbb24
Meet S. Patel <meet.s.patel.nj@gmail.com>
2021-01-07 17:58:37 -0500

    Wrote documentation for OrderBook and OrderBookLevel.

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['last',['last',['../struct_order_book.html#ab0ed366b34cbbbee04ff60720a75c82f',1,'OrderBook']]],
   ['level_5fvolume',['level_volume',['../struct_order_book_level.html#ae092a9f62fd808976bf15af2e60676bf',1,'OrderBookLevel']]],
   ['limit',['limit',['../_order_8h.html#aa8db2dc425ada39e309656f60a947e6baaa9f73eea60a006820d0f8768bc8a3fc',1,'Order.h']]],
   ['lowest_5fask',['lowest_ask',['../struct_order_book.html#a2a6cbfc50491eb02102fb48f88a1716e',1,'OrderBook']]]
